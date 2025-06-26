855e26d1a42921fb27c77b78f6a61e19ce79428f
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 17:32:49 -0300

    Initial front-end commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Band rehearsal management. Scheduling, reminders, attendance, setlists.</p>
+    </div>
+  );
+}
+export default App;
